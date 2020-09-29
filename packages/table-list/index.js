954fb83c74e957311e56463a413dc76ff50c1510
Louis <louiscd@126.com>
2020-09-29 22:43:01 +0800

    [fix]-[test 【formCreate】修复]

--- packages/table-list/index.js
+++ packages/table-list/index.js
@@ -1,10 +1,8 @@
 // 导入组件，组件必须声明 name
 import AwesomeTableList from './src/AwesomeTableList.vue';
-import formCreate from '@form-create/element-ui';
 
 // 为组件提供 install 安装方法，供按需引入
 AwesomeTableList.install = function(Vue) {
-  Vue.use(formCreate);
   Vue.component(AwesomeTableList.name, AwesomeTableList);
 };
 
